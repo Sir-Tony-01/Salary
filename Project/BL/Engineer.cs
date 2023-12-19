ef27b3fe103c4ba0aff548db199edc9195055f43
Антон Сиривля <sirivliaa@gmail.com>
2023-12-19 21:20:33 +0600

    first commit

---
+++ Project/BL/Engineer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Engineer : Person
+    {
+        public string position { get; set; }
+        public int hours { get; set; }
+        public int overHours { get; set; }
+        public int bonus { get; set; }
+
+        public Engineer()
+            : base()
+        {
+
+        }
+        public Engineer(string firstName, string SecondName, string Patnymic, string Position, int Hours, int OverHours, int Bonus)
+            : base(firstName, SecondName, Patnymic)
+        {
+            this.position = Position;
+            this.hours = Hours;
+            this.overHours = OverHours;
+            this.bonus = Bonus;
+        }
+        public override double CalculateSalary(int hours, int overhours, int bonus)
+        {
+            return 300 * hours + 500 * overhours + 0 * bonus;
+        }
+    }
+}
---
+++ Project/BL/Manager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Manager : Person
+    {
+        public string position { get; set; }
+        public int hours { get; set; }
+        public int bonus { get; set; }
+        public int overhours { get; set; }
+
+        public Manager()
+            : base()
+        {
+
+        }
+        public Manager(string firstName, string SecondName, string Patnymic, string Position, int Hours, int OverHours, int Bonus)
+            : base(firstName, SecondName, Patnymic)
+        {
+            this.position = Position;
+            this.hours = Hours;
+            this.bonus = Bonus;
+            this.overhours = OverHours;
+        }
+        public override double CalculateSalary(int hours, int overhours, int bonus)
+        {
+            return 250 * hours + 350 * overhours + 1000 * bonus;
+        }
+    }
+}
